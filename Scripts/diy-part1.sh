3e15f5cf528ff9af34e51f830dcd795c8637baf7
Leslie-Wong <manyu34@gmail.com>
2020-11-30 22:53:11 +0800

    Update diy-part1.sh

--- Scripts/diy-part1.sh
+++ Scripts/diy-part1.sh
@@ -13,8 +13,7 @@
 
 # Lienol/openwrt-package
 #sed -i '$a src-git lienol https://github.com/Lancenas/lienol-openwrt-package.git' feeds.conf.default
-sed -i '$a src-git luci-lienol https://github.com/Lienol/openwrt-luci.git;17.01' feeds.conf.default
-sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package.git;main' feeds.conf.default
-sed -i '$a src-git passwall https://github.com/xiaorouji/openwrt-passwall.git;main' feeds.conf.default
+sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package.git' feeds.conf.default
+sed -i '$a src-git passwall https://github.com/xiaorouji/openwrt-passwall.git' feeds.conf.default
 sed -i '$a src-git freifunk https://github.com/freifunk/openwrt-packages.git;openwrt-19.07' feeds.conf.default
 #sed -i '$a src-git luci-app-adguardhome https://github.com/rufengsuixing/luci-app-adguardhome' feeds.conf.default
