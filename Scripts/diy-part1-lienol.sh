3599b4a2da02bb9884aa119b6300a07462bc989e
Leslie-Wong <manyu34@gmail.com>
2020-12-01 09:44:10 +0800

    Create diy-part1-lienol.sh

---
+++ Scripts/diy-part1-lienol.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#=============================================================
+# https://github.com/P3TERX/Actions-OpenWrt
+# File name: diy-part1.sh
+# Description: OpenWrt DIY script part 1 (Before Update feeds)
+# Lisence: MIT
+# Author: P3TERX
+# Blog: https://p3terx.com
+#=============================================================
+
+# fw876/helloworld
+#sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
+
+# Lienol/openwrt-package
+#sed -i '$a src-git lienol https://github.com/Lancenas/lienol-openwrt-package.git' feeds.conf.default
+#sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package.git' feeds.conf.default
+#sed -i '$a src-git passwall https://github.com/xiaorouji/openwrt-passwall.git' feeds.conf.default
+#sed -i '$a src-git luci-app-adguardhome https://github.com/rufengsuixing/luci-app-adguardhome' feeds.conf.default
