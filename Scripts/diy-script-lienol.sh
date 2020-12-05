5a677077985083539eaf6a58319652ae635c43f5
Leslie-Wong <manyu34@gmail.com>
2020-12-05 14:21:07 +0800

    Update diy-script-lienol.sh

--- Scripts/diy-script-lienol.sh
+++ Scripts/diy-script-lienol.sh
@@ -18,10 +18,15 @@ Replace_File system package/base-files/files/etc/config
 #Replace_File AutoUpdate.sh package/base-files/files/bin
 Replace_File banner package/base-files/files/etc
 
-ExtraPackages svn network/services dnsmasq https://github.com/openwrt/openwrt/trunk/package/network/services
-ExtraPackages svn network/services dropbear https://github.com/openwrt/openwrt/trunk/package/network/services
-ExtraPackages svn network/services ppp https://github.com/openwrt/openwrt/trunk/package/network/services
-ExtraPackages svn network/services hostapd https://github.com/openwrt/openwrt/trunk/package/network/services
+# ExtraPackages svn network/services dnsmasq https://github.com/openwrt/openwrt/trunk/package/network/services
+# ExtraPackages svn network/services dropbear https://github.com/openwrt/openwrt/trunk/package/network/services
+# ExtraPackages svn network/services ppp https://github.com/openwrt/openwrt/trunk/package/network/services
+# ExtraPackages svn network/services hostapd https://github.com/openwrt/openwrt/trunk/package/network/services
+ExtraPackages svn network/services dnsmasq https://github.com/openwrt/openwrt/tree/master/package/network/services
+ExtraPackages svn network/services dropbear https://github.com/openwrt/openwrt/tree/master/package/network/services
+ExtraPackages svn network/services ppp https://github.com/openwrt/openwrt/tree/master/package/network/services
+ExtraPackages svn network/services hostapd https://github.com/openwrt/openwrt/tree/master/package/network/services
+ExtraPackages svn kernel mac80211 https://github.com/openwrt/openwrt/tree/master/package/kernel
 # ExtraPackages svn kernel mt76 https://github.com/openwrt/openwrt/trunk/package/kernel
 
 #ExtraPackages git lean luci-app-autoupdate https://github.com/Hyy2001X main
