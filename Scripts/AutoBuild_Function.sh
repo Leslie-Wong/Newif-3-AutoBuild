e00a0411aa6ff2b89795b504225d2b36995bd01c
Leslie-Wong <manyu34@gmail.com>
2021-02-21 00:04:33 +0800

    Update AutoBuild_Function.sh

--- Scripts/AutoBuild_Function.sh
+++ Scripts/AutoBuild_Function.sh
@@ -4,29 +4,64 @@
 # AutoBuild Functions
 
 GET_TARGET_INFO() {
+	Home=${GITHUB_WORKSPACE}/openwrt
+	echo "Home Path: ${Home}"
 	[ -f ${GITHUB_WORKSPACE}/Openwrt.info ] && . ${GITHUB_WORKSPACE}/Openwrt.info
 	Default_File="package/lean/default-settings/files/zzz-default-settings"
 	[ -f ${Default_File} ] && Lede_Version="$(egrep -o "R[0-9]+\.[0-9]+\.[0-9]+" ${Default_File})"
 	[[ -z ${Lede_Version} ]] && Lede_Version="Openwrt"
 	Openwrt_Version="${Lede_Version}-${Compile_Date}"
-	TARGET_PROFILE="$(egrep -o "CONFIG_TARGET.*DEVICE.*=y" .config | sed -r 's/.*DEVICE_(.*)=y/\1/')"
+	x86_Test="$(egrep -o "CONFIG_TARGET.*DEVICE.*=y" .config | sed -r 's/CONFIG_TARGET_(.*)_DEVICE_(.*)=y/\1/')"
+	if [[ "${x86_Test}" == "x86_64" ]];then
+		TARGET_PROFILE="x86_64"
+	else
+		TARGET_PROFILE="$(egrep -o "CONFIG_TARGET.*DEVICE.*=y" .config | sed -r 's/.*DEVICE_(.*)=y/\1/')"
+	fi
 	[[ -z "${TARGET_PROFILE}" ]] && TARGET_PROFILE="${Default_Device}"
+	case "${TARGET_PROFILE}" in
+	x86_64)
+		grep "CONFIG_TARGET_IMAGES_GZIP=y" ${Home}/.config
+		if [[ ! $? -ne 0 ]];then
+			Firmware_sfx="img.gz"
+		else
+			Firmware_sfx="img"
+		fi
+	;;
+	*)
+		Firmware_sfx="bin"
+	;;
+	esac
 	TARGET_BOARD="$(awk -F '[="]+' '/TARGET_BOARD/{print $2}' .config)"
 	TARGET_SUBTARGET="$(awk -F '[="]+' '/TARGET_SUBTARGET/{print $2}' .config)"
 	Github_Repo="$(grep "https://github.com/[a-zA-Z0-9]" ${GITHUB_WORKSPACE}/.git/config | cut -c8-100)"
+	AutoBuild_Info=${GITHUB_WORKSPACE}/openwrt/package/base-files/files/etc/openwrt_info
 }
 
 Diy_Part1_Base() {
 	Diy_Core
 	Mkdir package/lean
+	Replace_File Customize/banner package/base-files/files/etc
+	Replace_File Customize/mac80211.sh package/kernel/mac80211/files/lib/wifi
 	if [[ "${INCLUDE_SSR_Plus}" == "true" ]];then
 		ExtraPackages git lean helloworld https://github.com/fw876 master
 		sed -i 's/143/143,25,5222/' package/lean/helloworld/luci-app-ssr-plus/root/etc/init.d/shadowsocksr
 	fi
-	if [[ "${INCLUDE_Latest_Xray}" == "true" ]];then
-		Update_Makefile xray package/lean/helloworld/xray
-		Update_Makefile v2ray package/lean/v2ray
-		Update_Makefile v2ray-plugin package/lean/v2ray-plugin
+	if [[ "${INCLUDE_HelloWorld}" == "true" ]];then
+		ExtraPackages git lean luci-app-vssr https://github.com/jerrykuku master
+		ExtraPackages git lean lua-maxminddb https://github.com/jerrykuku master
+	fi
+	if [[ "${INCLUDE_Bypass}" == "true" ]];then
+		ExtraPackages git other luci-app-bypass https://github.com/garypang13 main
+		find package/*/ feeds/*/ -maxdepth 2 -path "*luci-app-bypass/Makefile" | xargs -i sed -i 's/shadowsocksr-libev-ssr-redir/shadowsocksr-libev-alt/g' {}
+		find package/*/ feeds/*/ -maxdepth 2 -path "*luci-app-bypass/Makefile" | xargs -i sed -i 's/shadowsocksr-libev-ssr-server/shadowsocksr-libev-server/g' {}
+	fi
+	if [[ "${INCLUDE_OpenClash}" == "true" ]];then
+		ExtraPackages git other OpenClash https://github.com/vernesong master
+	fi
+	if [[ "${INCLUDE_Keep_Latest_Xray}" == "true" ]];then
+		Update_Makefile xray-core package/lean/helloworld/xray-core
+		# Update_Makefile v2ray package/lean/v2ray
+		# Update_Makefile v2ray-plugin package/lean/v2ray-plugin
 	fi
 	if [[ "${INCLUDE_AutoBuild_Tools}" == "true" ]];then
 		Replace_File Scripts/AutoBuild_Tools.sh package/base-files/files/bin
@@ -42,6 +77,9 @@ Diy_Part1_Base() {
 Diy_Part2_Base() {
 	Diy_Core
 	GET_TARGET_INFO
+	Replace_File Customize/uhttpd.po feeds/luci/applications/luci-app-uhttpd/po/zh-cn
+	Replace_File Customize/webadmin.po package/lean/luci-app-webadmin/po/zh-cn
+	Replace_File Customize/mwan3.config package/feeds/packages/mwan3/files/etc/config mwan3
 	if [[ "${INCLUDE_Enable_FirewallPort_53}" == "true" ]];then
 		[ -f "${Default_File}" ] && sed -i "s?iptables?#iptables?g" ${Default_File} > /dev/null 2>&1
 	fi
@@ -63,23 +101,56 @@ Diy_Part2_Base() {
 	echo "Router: ${TARGET_PROFILE}"
 	echo "Github: ${Github_Repo}"
 	[ -f "$Default_File" ] && sed -i "s?${Lede_Version}?${Lede_Version} Compiled by ${Author} [${Display_Date}]?g" $Default_File
-	echo "${Openwrt_Version}" > package/base-files/files/etc/openwrt_info
-	echo "${Github_Repo}" >> package/base-files/files/etc/openwrt_info
-	echo "${TARGET_PROFILE}" >> package/base-files/files/etc/openwrt_info
+	echo "${Openwrt_Version}" > ${AutoBuild_Info}
+	echo "${Github_Repo}" >> ${AutoBuild_Info}
+	echo "${TARGET_PROFILE}" >> ${AutoBuild_Info}
+	echo "Firmware Type: ${Firmware_sfx}"
+	echo "Writting Type: ${Firmware_sfx} to ${AutoBuild_Info} ..."
+	echo "${Firmware_sfx}" >> ${AutoBuild_Info}
+	
 }
 
 Diy_Part3_Base() {
 	Diy_Core
 	GET_TARGET_INFO
-	Default_Firmware="openwrt-${TARGET_BOARD}-${TARGET_SUBTARGET}-${TARGET_PROFILE}-squashfs-sysupgrade.bin"
-	AutoBuild_Firmware="AutoBuild-${TARGET_PROFILE}-${Openwrt_Version}.bin"
-	AutoBuild_Detail="AutoBuild-${TARGET_PROFILE}-${Openwrt_Version}.detail"
+	Firmware_Path="bin/targets/${TARGET_BOARD}/${TARGET_SUBTARGET}"
 	Mkdir bin/Firmware
-	echo "Firmware: ${AutoBuild_Firmware}"
-	mv -f bin/targets/"${TARGET_BOARD}/${TARGET_SUBTARGET}/${Default_Firmware}" bin/Firmware/"${AutoBuild_Firmware}"
-	_MD5=$(md5sum bin/Firmware/${AutoBuild_Firmware} | cut -d ' ' -f1)
-	_SHA256=$(sha256sum bin/Firmware/${AutoBuild_Firmware} | cut -d ' ' -f1)
-	echo -e "\nMD5:${_MD5}\nSHA256:${_SHA256}" > bin/Firmware/"${AutoBuild_Detail}"
+	case "${TARGET_PROFILE}" in
+	x86_64)
+		cd ${Firmware_Path}
+		Firmware=openwrt-${TARGET_BOARD}-${TARGET_SUBTARGET}-generic-squashfs-combined
+		AutoBuild_Firmware="AutoBuild-${TARGET_PROFILE}-${Openwrt_Version}"
+		if [ -f "${Firmware}${Firmware_sfx}" ];then
+			_MD5=$(md5sum ${Firmware}${Firmware_sfx} | cut -d ' ' -f1)
+			_SHA256=$(sha256sum ${Firmware}${Firmware_sfx} | cut -d ' ' -f1)
+			touch ${Home}/bin/Firmware/${AutoBuild_Firmware}.detail
+			echo -e "\nMD5:${_MD5}\nSHA256:${_SHA256}" > ${Home}/bin/Firmware/${AutoBuild_Firmware}-Legacy.detail
+			mv -f ${Firmware}.${Firmware_sfx} ${Home}/bin/Firmware/${AutoBuild_Firmware}-Legacy${Firmware_sfx}
+			echo "Legacy Firmware is detected !"
+		fi
+		if [ -f "${Firmware}-efi${Firmware_sfx}" ];then
+			_MD5=$(md5sum ${Firmware}-efi${Firmware_sfx} | cut -d ' ' -f1)
+			_SHA256=$(sha256sum ${Firmware}-efi${Firmware_sfx} | cut -d ' ' -f1)
+			touch ${Home}/bin/Firmware/${AutoBuild_Firmware}.detail
+			echo -e "\nMD5:${_MD5}\nSHA256:${_SHA256}" > ${Home}/bin/Firmware/${AutoBuild_Firmware}-UEFI.detail
+			cp ${Firmware}-efi.${Firmware_sfx} ${Home}/bin/Firmware/${AutoBuild_Firmware}-UEFI${Firmware_sfx}
+			echo "UEFI Firmware is detected !"
+		fi
+	;;
+	*)
+		cd ${Home}
+		Default_Firmware="openwrt-${TARGET_BOARD}-${TARGET_SUBTARGET}-${TARGET_PROFILE}-squashfs-sysupgrade.${Firmware_sfx}"
+		AutoBuild_Firmware="AutoBuild-${TARGET_PROFILE}-${Openwrt_Version}.${Firmware_sfx}"
+		AutoBuild_Detail="AutoBuild-${TARGET_PROFILE}-${Openwrt_Version}.detail"
+		echo "Firmware: ${AutoBuild_Firmware}"
+		mv -f ${Firmware_Path}/${Default_Firmware} bin/Firmware/${AutoBuild_Firmware}
+		_MD5=$(md5sum bin/Firmware/${AutoBuild_Firmware} | cut -d ' ' -f1)
+		_SHA256=$(sha256sum bin/Firmware/${AutoBuild_Firmware} | cut -d ' ' -f1)
+		echo -e "\nMD5:${_MD5}\nSHA256:${_SHA256}" > bin/Firmware/${AutoBuild_Detail}
+	;;
+	esac
+	cd ${Home}
+	echo "Actions Avaliable: $(df -h | grep "/dev/root" | awk '{printf $4}')"
 }
 
 Mkdir() {
@@ -109,8 +180,7 @@ ExtraPackages() {
 		git)
 		
 			if [[ -z "${REPO_BRANCH}" ]];then
-				echo "[$(date "+%H:%M:%S")] Missing important options,skip check out..."
-				break
+				REPO_BRANCH="master"
 			fi
 			git clone -b ${REPO_BRANCH} ${REPO_URL}/${PKG_NAME} ${PKG_NAME} > /dev/null 2>&1
 		;;
@@ -158,28 +228,34 @@ Replace_File() {
 }
 
 Update_Makefile() {
-	PKG_NAME="$1"
-	Makefile="$2/Makefile"
+	PKG_NAME=${1}
+	Makefile=${2}/Makefile
+	[ -f "/tmp/tmp_file" ] && rm -f /tmp/tmp_file
 	if [ -f "${Makefile}" ];then
 		PKG_URL_MAIN="$(grep "PKG_SOURCE_URL:=" ${Makefile} | cut -c17-100)"
 		_process1=${PKG_URL_MAIN##*com/}
 		_process2=${_process1%%/tar*}
 		api_URL="https://api.github.com/repos/${_process2}/releases"
-		PKG_DL_URL="https://codeload.github.com/${_process2}/tar.gz/v"
+		PKG_SOURCE_URL="$(grep "PKG_SOURCE_URL:=" ${Makefile} | cut -c17-100)"
+		PKG_DL_URL="${PKG_SOURCE_URL%\$(\PKG_VERSION*}"
 		Offical_Version="$(curl -s ${api_URL} 2>/dev/null | grep 'tag_name' | egrep -o '[0-9].+[0-9.]+' | awk 'NR==1')"
-		if [[ -z "${Offical_Version}" ]] && [[ ! "$?" -eq 0 ]];then
-			echo "Failed to obtain the Offical version,skip update ..."
+		if [[ -z "${Offical_Version}" ]];then
+			echo "Failed to obtain the Offical version of [${PKG_NAME}],skip update ..."
 			return
 		fi
 		Source_Version="$(grep "PKG_VERSION:=" ${Makefile} | cut -c14-20)"
 		Source_HASH="$(grep "PKG_HASH:=" ${Makefile} | cut -c11-100)"
+		if [[ -z "${Source_Version}" ]] || [[ -z "${Source_HASH}" ]];then
+			echo "Failed to obtain the Source version or HASH,skip update ..."
+			return
+		fi
 		echo -e "Current ${PKG_NAME} version: ${Source_Version}\nOffical ${PKG_NAME} version: ${Offical_Version}"
-		if [[ ! "${Source_Version}" == "${Offical_Version}" ]] && [[ ! "$?" -eq 0 ]];then
+		if [[ ! "${Source_Version}" == "${Offical_Version}" ]];then
 			echo -e "Updating package ${PKG_NAME} [${Source_Version}] to [${Offical_Version}] ..."
 			sed -i "s?PKG_VERSION:=${Source_Version}?PKG_VERSION:=${Offical_Version}?g" ${Makefile}
 			wget -q "${PKG_DL_URL}${Offical_Version}?" -O /tmp/tmp_file
 			if [[ "$?" -eq 0 ]];then
-				Offical_HASH=$(sha256sum /tmp/tmp_file | cut -d ' ' -f1)
+				Offical_HASH="$(sha256sum /tmp/tmp_file | cut -d ' ' -f1)"
 				sed -i "s?PKG_HASH:=${Source_HASH}?PKG_HASH:=${Offical_HASH}?g" ${Makefile}
 			else
 				echo "Failed to update the package [${PKG_NAME}],skip update ..."
@@ -188,5 +264,5 @@ Update_Makefile() {
 	else
 		echo "Package ${PKG_NAME} is not detected,skip update ..."
 	fi
-	unset _process1 _process2 Offical_Version
+	unset _process1 _process2 Offical_Version Source_Version
 }
